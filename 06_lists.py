3665e24278552bc2987ce4aa7f00551c4f807715
Sohini Ball <ballsohini939@gmail.com>
2026-08-01 12:43:23 +0530

    Add lists practice program

---
+++ 06_lists.py
@@ -0,0 +1,6 @@
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+print("My fruits:", fruits)
+print("First fruit:", fruits[0])
+print("Second fruit:", fruits[1])
+fruits.append("Grapes")
+print("After adding Grapes:", fruits)
