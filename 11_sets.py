6b16e3b537ad595c6bd232b29132707fffa21efa
Sohini Ball <ballsohini939@gmail.com>
2026-08-01 13:27:27 +0530

    Add sets practice program

---
+++ 11_sets.py
@@ -0,0 +1,7 @@
+numbers = {10, 20, 30, 20, 40, 60}
+print("Original set:", numbers)
+numbers.add(50)
+print("After adding 50:", numbers)
+numbers.remove(20)
+print("After removing 20:", numbers)
+print("Number of elements:", len(numbers))
