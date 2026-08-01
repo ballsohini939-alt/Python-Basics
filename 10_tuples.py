b3fe0146fc333d8f78c0a6644d73df99d923227e
Sohini Ball <ballsohini939@gmail.com>
2026-08-01 13:02:15 +0530

    Add tuples practice program

---
+++ 10_tuples.py
@@ -0,0 +1,8 @@
+student = ("Sohini", 19, "Computer Science", 8.92)
+print("~~~Student information~~~:", student)
+
+print("Name:", student[0])
+print("Age:", student[1])
+print("Course:", student[2])
+print("CGPA:", student[3])
+print("Number of items:", len(student))
