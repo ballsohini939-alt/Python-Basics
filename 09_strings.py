85b6114ec4820e51220e3b36c91632dbcf1e8699
Sohini Ball <ballsohini939@gmail.com>
2026-08-01 12:59:41 +0530

    Add strings practice program

---
+++ 09_strings.py
@@ -0,0 +1,11 @@
+text = 'Python Programming'
+
+print("Original:", text)
+print("Length:", len(text))
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("First character:", text[0])
+print("Last character:", text[-1])
+print("First 6 characters:", text[:6])
+
+print("Contains Python:", "Python" in text)
