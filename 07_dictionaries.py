754ed7d9936aff55cd88c340c1a5421b0deaff31
Sohini Ball <ballsohini939@gmail.com>
2026-08-01 12:48:13 +0530

    Add dictionaries practice program

---
+++ 07_dictionaries.py
@@ -0,0 +1,15 @@
+student = {
+    "name": "Sohini",
+    "age": 19,
+    "college": "Asansol Engineering College",
+    "course": "Computer Science"
+}
+
+print("---Student Information---")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("College:", student["college"])
+print("Course:", student["course"])
+
+student["year"] = 2
+print("Updated Information:", student)
